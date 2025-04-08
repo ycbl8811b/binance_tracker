556d7e135ed20dac1e443c508de49d12e1e82792
ycbl8811b <t861974691@gmail.com>
2025-04-08 14:38:48 +0300

    Delete useless dirs

--- backend/app/handlers/operations.py
+++
@@ -1,8 +0,0 @@
-from calculations.calculation import (
-    sum_tickers, subdivide_tickers,
-    multiply_tickers, divide_tickers
-)
-
-
-def solve_formula(formula: str) -> float:
-    pass
--- backend/app/handlers/ticker.py
+++
@@ -1,5 +0,0 @@
-def get_price(ticker: dict) -> float:
-    return float(ticker['price'])
-
-def get_symbol(ticker: dict) -> str:
-    return ticker['symbol']
